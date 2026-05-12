b1802330191611b700c5f08c9789a828d1075d73
conditionull <uuhlina@icloud.com>
2026-05-12 16:47:05 -0400

    remove animation for hyprpicker & screenshot

--- hyprland.lua
+++ hyprland.lua
@@ -159,6 +159,15 @@ hl.layer_rule({
   no_anim = true,
 })
 
+hl.layer_rule({
+  match = { namespace = "selection" },
+  no_anim = true,
+})
+
+hl.layer_rule({
+  match = { namespace = "hyprpicker" },
+  no_anim = true,
+})
 
 ----------------------------
 ---- MAKO NOTIFICATIONS ----
